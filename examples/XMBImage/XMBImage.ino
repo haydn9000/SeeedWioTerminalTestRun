27b89c65c7883151e4a8c834a2f08f7c98d4aabd
Noir <haydnady@gmail.com>
2020-10-03 17:27:31 -0400

    Hex image displaying working

--- examples/XMBImage/XMBImage.ino
+++ examples/XMBImage/XMBImage.ino
@@ -14,40 +14,45 @@
 TFT_eSPI tft;
 TFT_eSprite spr = TFT_eSprite(&tft); // Buffer
 
-//static LCDBackLight backLight;
-//int maxBrightness = backLight.getMaxBrightness(); // Max brightness
+char optionTest = 'C';
 
-void setup() 
+//========================================================================= SETUP
+void setup()
 {
-    tft.begin();
-    tft.setRotation(3);
-    spr.createSprite(TFT_HEIGHT, TFT_WIDTH); // Create buffer
-
-//    backLight.initialize();
-//    backLight.setBrightness(50); // Max brightness is 100.
-
-
-    // Draw the icons
-    tft.pushImage(100, 100, 100, 95, homeIcon);
-
-    // Top 3 button inputs, far right button is A, middle B, left C.
-    pinMode(WIO_KEY_A, INPUT);
-    pinMode(WIO_KEY_B, INPUT);
-    pinMode(WIO_KEY_C, INPUT);
-}
-
-void drawHome()
-{
+  tft.begin();
+  tft.setRotation(3);
+  spr.createSprite(TFT_HEIGHT, TFT_WIDTH); // Create buffer
+  
   spr.fillSprite(tft.color565(255, 0, 0));
-  spr.setTextSize(2);
-
-//  spr.drawXbm(110, 0, 100, 95, houseIcon);
-
+  
+  // Draw the icons
+  spr.pushImage(0, 0, 320, 240, spongebob);
+//  displayImage();
+    
+  
   spr.pushSprite(0, 0);
-  delay(200);
+
+  
+  // Top 3 button inputs, far right button is A, middle B, left C.
+  pinMode(WIO_KEY_A, INPUT);
+  pinMode(WIO_KEY_B, INPUT);
+  pinMode(WIO_KEY_C, INPUT);
+  // 5 Way switch
+  pinMode(WIO_5S_UP, INPUT_PULLUP);
+  pinMode(WIO_5S_DOWN, INPUT_PULLUP);
+  pinMode(WIO_5S_LEFT, INPUT_PULLUP);
+  pinMode(WIO_5S_RIGHT, INPUT_PULLUP);
+  pinMode(WIO_5S_PRESS, INPUT_PULLUP);
 }
 
-void loop() 
+//========================================================================= Functions
+void displayImage()
 {
+  
+}
+
+//========================================================================= LOOP
+void loop()
+{ 
   
 }
