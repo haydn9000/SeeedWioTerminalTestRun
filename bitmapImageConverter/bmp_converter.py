ae8e08c2af7e6ad6b7e083ef431fc338279a61d6
Haydn <haydnady@gmail.com>
2021-05-30 12:03:45 -0400

    Qthread class added

--- bitmapImageConverter/bmp_converter.py
+++ bitmapImageConverter/bmp_converter.py
@@ -1,3 +1,4 @@
+from PySide6.QtCore import QThread, Signal
 from PIL import Image
 import os
 
@@ -63,7 +64,46 @@ class ConvertBMP:
             print("Invalid input!")
 
 
+class BMPProcessingThread(QThread):
+    message = Signal(str)
+
+    def __init__(self, rgbType, saveDirName, parent=None):
+        QThread.__init__(self, parent)
+
+        self.rgbType = rgbType
+        self.saveDirName = saveDirName
+
+
+    """
+    - The starting point for the thread. After calling start(), 
+      QThreads begin executing in run(). By default, 
+      run() starts the event loop by calling exec() 
+      and runs a Qt event loop inside the thread.
+    - https://doc.qt.io/qt-6/qthread.html#details
+    """
+    def run(self):
+        try:
+            self.convertBMP()
+
+        except Exception as e:
+            self.message.emit(f"<b style=color:LightCoral;>{e}<br></b>")
+
+
+    def stop(self):
+        self.quit()
+        self.wait()
+        self.deleteLater()  # May not be needed, added to delete object when finished..
+
+
+    # Processing function
+    def convertBMP(self):
+        convert = ConvertBMP()
+        rgbType = convert.rgb332 if (self.rgbType == 1) else convert.rgb565
+        convert.convert(rgbType, "bmp", self.saveDirName)
+        self.message.emit("Process complete")
+
+
 if __name__ == "__main__":
     convert = ConvertBMP()
-    rgbType, saveDirName = convert.choose();
+    rgbType, saveDirName = convert.choose()
     convert.convert(rgbType, "bmp", saveDirName)
