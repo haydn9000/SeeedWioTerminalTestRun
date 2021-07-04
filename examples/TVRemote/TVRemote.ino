d1b501e782769598615497e84fc16581bc2b89b5
Noir <haydnady@gmail.com>
2021-07-03 20:39:58 -0400

    WIP

--- examples/TVRemote/TVRemote.ino
+++ examples/TVRemote/TVRemote.ino
@@ -4,33 +4,42 @@
  *  capture the values. They will print in the serial monitor. Then you
  *  cut and paste that output into the appropriate section below.
  */
-#include <IRLibSendBase.h>    //We need the base code
-#include <IRLib_HashRaw.h>    //Only use raw sender
+#include <IRLibSendBase.h> // We need the base code
+#include <IRLib_HashRaw.h> // Only use raw sender
 
 IRsendRaw mySender;
 
-void setup() {
-  Serial.begin(9600);
-  delay(2000); while (!Serial); //delay for Leonardo
-  Serial.println(F("Every time you press a key is a serial monitor we will send."));
-}
 /* Cut and paste the output from "rawRecv.ino" below here. It will 
  * consist of a #define RAW_DATA_LEN statement and an array definition
  * beginning with "uint16_t rawData[RAW_DATA_LEN]= {…" and concludes
  * with "…,1000};"
  */
 
+uint16_t rawData[RAW_DATA_LEN] = {"... and concludes * with…, 1000"};
 
 /*
  * Cut-and-paste into the area above.
  */
-   
-void loop() {
-  if (Serial.read() != -1) {
-    //send a code every time a character is received from the 
-    // serial port. You could modify this sketch to send when you
-    // push a button connected to an digital input pin.
-    mySender.send(rawData,RAW_DATA_LEN,36);//Pass the buffer,length, optionally frequency
-    Serial.println(F("Sent signal."));
-  }
+
+
+void setup()
+{
+    Serial.begin(9600);
+    delay(2000);
+    while (!Serial)
+        ; //delay for Leonardo
+    Serial.println(F("Every time you press a key is a serial monitor we will send."));
+}
+
+
+void loop()
+{
+    if (Serial.read() != -1)
+    {
+        //send a code every time a character is received from the
+        // serial port. You could modify this sketch to send when you
+        // push a button connected to an digital input pin.
+        mySender.send(rawData, RAW_DATA_LEN, 36); //Pass the buffer,length, optionally frequency
+        Serial.println(F("Sent signal."));
+    }
 }
